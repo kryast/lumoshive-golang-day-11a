e381b0fc210d663bde48b1cbe5cfdfbd0da0a4fc
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-10-21 14:21:16 +0700

    func get id

--- mtk.go
+++ mtk.go
@@ -1,20 +1,34 @@
 package MTK
 
-func Tambah(a int, b int) int {
-	return a + b
+// func Tambah(a int, b int) int {
+// 	return a + b
+// }
+
+// func Kurang(a int, b int) int {
+// 	return a - b
+// }
+
+// func Kali(a int, b int) int {
+// 	return a * b
+// }
+
+// func Bagi(a int, b int) int {
+// 	if b == 0 {
+// 		return 0
+// 	}
+// 	return a / b
+// }
+
+type Data struct {
+	Id int
 }
 
-func Kurang(a int, b int) int {
-	return a - b
-}
+var DataAkun []Data
 
-func Kali(a int, b int) int {
-	return a * b
-}
-
-func Bagi(a int, b int) int {
-	if b == 0 {
-		return 0
+func GetId(length int) (Data, []Data) {
+	for i := 0; i < length; i++ {
+		DataAkun = append(DataAkun, Data{Id: i})
 	}
-	return a / b
+	return Data{}, DataAkun
+
 }
